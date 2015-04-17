264f693f6e02c3ff78fc4d822bbdaa861e069515
Jose Aureliano <Apple@junior-macbook.local>
2015-04-17 18:51:19 -0300

    first commit

---
+++ app/controllers/documents_controller.rb
@@ -0,0 +1,81 @@
+class DocumentsController < ApplicationController
+  before_action :set_document, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
+  before_action :authenticate_person!
+
+  # GET /documents
+  # GET /documents.json
+  def index
+    @documents = Document.where(user: @user)
+  end
+
+  # GET /documents/1
+  # GET /documents/1.json
+  def show
+  end
+
+  # GET /documents/new
+  def new
+    @document = Document.new
+  end
+
+  # GET /documents/1/edit
+  def edit
+  end
+
+  # POST /documents
+  # POST /documents.json
+  def create
+    @document = Document.new(document_params)
+    @document.user = @user
+    respond_to do |format|
+      if @document.save
+        @user.documents << @document
+        format.html { redirect_to user_documents_path(@user), notice: 'Document was successfully created.' }
+        format.json { render :show, status: :created, location: @document }
+      else
+        format.html { render :new }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /documents/1
+  # PATCH/PUT /documents/1.json
+  def update
+    respond_to do |format|
+      if @document.update(document_params)
+        format.html { redirect_to user_documents_path(@user), notice: 'Document was successfully updated.' }
+        format.json { render :show, status: :ok, location: @document }
+      else
+        format.html { render :edit }
+        format.json { render json: @document.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /documents/1
+  # DELETE /documents/1.json
+  def destroy
+    @document.destroy
+    respond_to do |format|
+      format.html { redirect_to user_documents_path(@user), notice: 'Document was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_document
+      @document = Document.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def document_params
+      params.require(:document).permit(:user_id, :title, :description)
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+end
---
+++ db/migrate/20150417204745_create_documents.rb
@@ -0,0 +1,11 @@
+class CreateDocuments < ActiveRecord::Migration
+  def change
+    create_table :documents do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
